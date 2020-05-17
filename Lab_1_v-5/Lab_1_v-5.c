63e0ded4dd274098fbca50986eaef0edf7ba4828
stalkervr <stalkervr@gmail.com>
2020-05-17 20:25:34 +0700

    Лабораторная работа № 1. Готово.

---
+++ Lab_1_v-5/Lab_1_v-5.c
@@ -0,0 +1,77 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+
+char* inner_search(char*, char*, char*);
+void search_wrapper(char*, char*, char*);
+
+int main()
+{
+    setlocale(LC_ALL, "RU-ru");
+    printf("----------------------------------------------------------------------------------------------- \n\n");
+    printf("            Лабораторная работа № 1 вариант № 5\n\n");
+    printf("  Задание : Функция находит в строке пары инвертированных фрагментов .\n");
+    printf("            например \"123apr\" и \"rpa321\"\  и возвращает указатель на первый.\n");
+    printf("            С помощью функции найти все пары.\n\n");
+    printf("----------------------------------------------------------------------------------------------- \n\n");
+    //  тестовые строки для поиска
+    char* str  = "xxx 123apr aaadffdz 123apr bbbdffd 123apr cccdffd rpa321 dfgdhxgnfgxh 123apr cccdfhn rpa321 xbf";
+    char* str1 = "xxx 123apr aaadffdz 123apr bbbdffd  dfgdhxgnfgxh 123apr cccdfhn  xbf";
+    char* str2 = "xxx  aaadffdz  bbbdffd rpa321 dfgdhxgnfgxh  cccdfhn rpa321 xbf";
+    char* str3 = "rpa321 xxx 123apr aaadffdz 123apr bbbdffd rpa321 dfgdhxgnfgxh 123apr cccdfhn rpa321 xbf";
+    
+    // фрагменты для поиска
+    char* _str1 = "123apr";
+    char* _str2 = "rpa321";
+
+    // тестирование работы функции
+    search_wrapper(str, _str1, _str2);
+    search_wrapper(str1, _str1, _str2);
+    search_wrapper(str2, _str1, _str2);
+    search_wrapper(str3, _str1, _str2);
+    getchar();
+
+    return 0;
+}
+
+void search_wrapper(char* str, char* _str1, char* _str2) 
+{
+    printf(">> Исходная строка для поиска \n%s\n\n", str);
+
+    int count = 0;
+    char* start_next_point = str;
+
+    while (strstr(start_next_point, _str1) && strstr(start_next_point, _str2))
+    {
+        if (strstr(start_next_point, _str2) < strstr(start_next_point, _str1)) 
+        {
+            start_next_point = strstr(start_next_point, _str1);
+        }
+        inner_search(start_next_point, _str1, _str2);
+        printf("%s\n", inner_search(start_next_point, _str1, _str2));
+        count++;
+        start_next_point = inner_search(start_next_point, _str1, _str2) + strlen(_str1);
+        inner_search(start_next_point, _str1, _str2);
+    }
+    printf("Поиск завершон ... найдено %d фрагментов \n\n",count);
+    printf("----------------------------------------------------------------------------------------------- \n\n", count);
+}
+
+char* inner_search(char* str, char* str1, char* str2)
+{
+    // указатель на фрагмент
+    char* pointer1 = (strstr(str, str1));
+    // указатель на инвертированный фрагмент
+    char* pointer2 = (strstr(str, str2));
+    // указатель на точку продолжения поиска
+    char* start_next_point = pointer1 + strlen(str1);
+  
+    while (pointer1 != NULL && strstr(start_next_point, str1) && (strstr(start_next_point, str1) < pointer2))
+    {
+        pointer1 = strstr(start_next_point, str1);
+        start_next_point = pointer1 + strlen(str1);
+    }
+    return pointer1;
+}
+
--- Lab_4_v-1/Lab_4_v-1.c
+++ Lab_4_v-1/Lab_4_v-1.c
@@ -1,53 +1,46 @@
-﻿// Lab_4_v-1.cpp
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <locale.h>
 
-double sum(int n, ...);
-double use_macr_sum(int n,  ...);
+double sum(int count, ...);
+double sum_use_macros(int count,  ...);
 
 int main()
 {
-    /*double a[] = { 1.1, 2.2, 3.3 };
+    setlocale(LC_ALL, "RU-ru");
 
-    int* pointer = &a;
-    double d;
-    d = *(double*)pointer;
-
-    for (int i = 0; i < 3; i++)
-    {
-        printf("Load num %f \n", d);
-        d++;
-    }*/
-
-
-    printf("Sum %3f\n", sum(3, 50.1, 40.3, 30.3));
-    printf("Sum %3f\n", use_macr_sum(4, 2.1, 1.1, 3.1, NULL));
+    printf("            Лабораторная работа № 4 вариант № 1\n\n");
+    printf("  Задание : Разработать функцию с переменным числом параметров.\n");
+    printf("            Для извлечения параметров из списка использовать операцию преобразования типа указателя.\n");
+    printf("            Целая переменная - счетчик, затем последовательность вещественных переменных.\n");
+    printf("            Функция возвращает сумму переменных.\n\n");
+    printf("  Сумма аргументов функция без макросов => %.3f\n", sum(3, 50.1, 40.3, 30.3));
+    printf("  Сумма аргументов функция с макросами  => %.3f\n", sum_use_macros(4, 2.1, 1.1, 3.1, NULL));
     getchar();
     return 0;
 }
 
-double sum(int n, ...)
+double sum(int count, ...)
 {
     double result = 0;
-    for (int* ptrArg = &n; n > 0;  n--)
+    for (int* ptrArg = &count; count > 0;  count--)
     {
         ptrArg++;
-        double d;
-        d = *(double*)ptrArg++;
-        printf("Load args -> %f\n", d);
-        result += d;
+        double ptrDouble;
+        ptrDouble = *(double*)ptrArg++;
+        result += ptrDouble;
     }
     return result;
 }
 
 
-double use_macr_sum(int n,  ...)
+double sum_use_macros(int count,  ...)
 {
     double result = 0;
     va_list sumArgs;
-    va_start(sumArgs, n);
-    for (int i = 0; i < n; i++)
+    va_start(sumArgs, count);
+    for (int i = 0; i < count; i++)
     {
         result += va_arg(sumArgs, double);
     }
